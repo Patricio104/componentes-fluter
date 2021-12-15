d7e8c87a5552544fbec5553a629eefac3c7d7a46
Patricio104 <patriciojuarez104@gmail.com>
2021-12-14 19:12:22 -0600

    Uso de FutureBuilder para los procesos en segundo plano

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -15,17 +15,50 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    print(menuProvider.opciones);
-    return ListView(
-      children: _listaItems(),
-    );
+    //print(menuProvider.opciones);
+    /* menuProvider.cargarData().then((opciones) {
+      //Aqui no es conveniente usarlo de esta forma, ya que como resuelve una tarea despues de unos segundo
+      //el cliente no le gustará, pensará que esta trabado la App
+      print('_lista');
+      print(opciones);
+    }); */
+
+    //Se usará FutureBuilder para una mejor ejecución de la tarea
+
+    return FutureBuilder(
+        future: menuProvider.cargarData(),
+        initialData: [],
+        builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error'));
+          }
+
+          if (!snapshot.hasData) {
+            return Center(child: Text('No hay data'));
+          }
+
+          return ListView(
+            children: _listaItems(snapshot.data),
+          );
+        });
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(title: Text('Hola que tal')),
-      ListTile(title: Text('Hola que tal')),
-      ListTile(title: Text('Hola que tal'))
-    ];
+  List<Widget> _listaItems(List<dynamic>? data) {
+    final List<Widget> opciones = [];
+
+    data?.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.ac_unit_rounded, color: Colors.blueAccent),
+        trailing: Icon(Icons.access_alarms_rounded, color: Colors.amber),
+        onTap: () {},
+      );
+
+      opciones
+        ..add(widgetTemp)
+        ..add(Divider());
+    });
+
+    return opciones;
   }
 }
